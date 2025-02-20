f1f25b0994a1515350a0d0b3a2ed199009ba9de6
Jkaps11 <kapsjosh11@gmail.com>
2025-02-20 15:24:38 -0500

    Separating sylesheet and component for cleaner code

---
+++ app/defaultStyles.ts
@@ -0,0 +1,56 @@
+import { COLORS } from "@/constants/theme";
+import { StyleSheet } from 'react-native';
+
+export const defaultStyles = StyleSheet.create({
+    container: {
+      position:"relative",
+      width:"100%",
+      height:"100%",
+      backgroundColor:COLORS.primary
+    },
+    textInput: {
+      backgroundColor:"white",
+      color:"black",
+      borderRadius:50,
+      width:"80%",
+      height:"40%",
+      textAlign: "center",
+    },
+    topBar: {
+      position: "absolute",
+      flex: 1,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-around",
+      backgroundColor:COLORS.primary,
+      top:0,
+      left:0,
+      width:"100%",
+      height:"10%",
+      zIndex:2,
+    },
+    bottomNavBar: {
+      position: "absolute",
+      flex: 1,
+      flexDirection: "row",
+      alignItems: "center",
+      justifyContent: "space-around",
+      backgroundColor:COLORS.primary,
+      borderRadius:50,
+      top: "80%",
+      left: "15%",
+      width: "70%",
+      height: "7.5%",
+      zIndex: 2,
+    },
+    map: {
+      position: "absolute",
+      left:0,
+      right:0,
+      bottom:0,
+      height:"100%",
+      width:"100%",
+      top:0,
+      zIndex: 1
+    }
+  })
--- app/index.tsx
+++ app/index.tsx
@@ -4,8 +4,9 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { MapView } from "@maplibre/maplibre-react-native";
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { StyleSheet, TextInput, View } from "react-native";
+import { TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { defaultStyles } from "./defaultStyles";
 
 export default function Index() {
   const [placeSearcherd, setPlaceSearched] = useState<string>("")
@@ -37,57 +38,3 @@ export default function Index() {
     </SafeAreaView>
     );
 }
-
-const defaultStyles = StyleSheet.create({
-  container: {
-    position:"relative",
-    width:"100%",
-    height:"100%",
-    backgroundColor:COLORS.primary
-  },
-  textInput: {
-    backgroundColor:"white",
-    color:"black",
-    borderRadius:50,
-    width:"80%",
-    height:"40%",
-    textAlign: "center",
-  },
-  topBar: {
-    position: "absolute",
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around",
-    backgroundColor:COLORS.primary,
-    top:0,
-    left:0,
-    width:"100%",
-    height:"10%",
-    zIndex:2,
-  },
-  bottomNavBar: {
-    position: "absolute",
-    flex: 1,
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-around",
-    backgroundColor:COLORS.primary,
-    borderRadius:50,
-    top: "80%",
-    left: "15%",
-    width: "70%",
-    height: "7.5%",
-    zIndex: 2,
-  },
-  map: {
-    position: "absolute",
-    left:0,
-    right:0,
-    bottom:0,
-    height:"100%",
-    width:"100%",
-    top:0,
-    zIndex: 1
-  }
-})
