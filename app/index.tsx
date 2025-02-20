c59559e4f219c8584f8789050612c970dc1c052c
Jkaps11 <kapsjosh11@gmail.com>
2025-02-20 15:49:26 -0500

    adding formater

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,16 @@
-import { COLORS } from "@/constants/theme";
-import Entypo from '@expo/vector-icons/Entypo';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { MapView } from "@maplibre/maplibre-react-native";
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import { TextInput, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-import { defaultStyles } from "./defaultStyles";
+import { COLORS } from '@/constants/theme'
+import Entypo from '@expo/vector-icons/Entypo'
+import FontAwesome from '@expo/vector-icons/FontAwesome'
+import { MapView } from '@maplibre/maplibre-react-native'
+import { StatusBar } from 'expo-status-bar'
+import { useState } from 'react'
+import { TextInput, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { defaultStyles } from './defaultStyles'
 
 export default function Index() {
-  const [placeSearcherd, setPlaceSearched] = useState<string>("")
-  const [search, setSearch] = useState<string>("");
+  const [placeSearcherd, setPlaceSearched] = useState<string>('')
+  const [search, setSearch] = useState<string>('')
 
   const handleSearch = () => {
     setSearch(placeSearcherd)
@@ -18,9 +18,15 @@ export default function Index() {
 
   return (
     <SafeAreaView style={defaultStyles.container}>
-      <StatusBar hidden={true}/>
+      <StatusBar hidden={true} />
       <View style={defaultStyles.topBar}>
-      <Entypo.Button name="menu" size={30} color={COLORS.defaultText} backgroundColor="#00000000" iconStyle={{margin:0}}/>
+        <Entypo.Button
+          name="menu"
+          size={30}
+          color={COLORS.defaultText}
+          backgroundColor="#00000000"
+          iconStyle={{ margin: 0 }}
+        />
         <TextInput
           style={defaultStyles.textInput}
           autoCorrect={true}
@@ -31,10 +37,20 @@ export default function Index() {
         />
       </View>
       <View style={defaultStyles.bottomNavBar}>
-        <FontAwesome.Button name="search" size={30} color={COLORS.defaultText} backgroundColor="#00000000"/>
-        <FontAwesome.Button name="filter" size={30} color={COLORS.defaultText} backgroundColor="#00000000"/>
+        <FontAwesome.Button
+          name="search"
+          size={30}
+          color={COLORS.defaultText}
+          backgroundColor="#00000000"
+        />
+        <FontAwesome.Button
+          name="filter"
+          size={30}
+          color={COLORS.defaultText}
+          backgroundColor="#00000000"
+        />
       </View>
       <MapView style={defaultStyles.map} />
     </SafeAreaView>
-    );
+  )
 }
