61a72a22b8b430e1174f21e1323a583c07261738
Jkaps11 <kapsjosh11@gmail.com>
2025-02-26 15:44:38 -0500

    formatter for windows

---
+++ formatter.bat
@@ -0,0 +1,9 @@
+@echo off
+
+:: Run Prettier to format the code
+echo Running Prettier to format the code...
+npm run format
+
+:: Run ESLint to lint and fix the code
+echo Running ESLint to lint and fix the code...
+npm run lint
